a4e83af0344e254d55b734a5fe546aa2745c7e90
AFZAL REYAZ <afjalreyaz@gmail.com>
2025-11-13 17:58:13 +0530

    cors commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup=async(req,res)=>{
     try{
@@ -32,6 +33,48 @@ export const signup=async(req,res)=>{
     }
 
 }
+
+export const login= async(req,res)=>{
+    try{
+        const { email, password } = req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invailid User or Password"});
+        }
+        const isMatch=await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"invailid User or Password"});
+        }
+        
+        const token = jwt.sign(
+            {id:user._id, email:user.email},
+            JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+
+        // 5️⃣ Send success response
+            return res.status(200).json({
+            message: "Login successful ✅",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            });
+
+    }catch(err){
+        console.log("Login error",err);
+        res.status(500).json({message:"Server Error"});
+    }
+
+}
+
+
+
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
-import { signup } from "../controllers/userController.js";
+import { signup,login } from "../controllers/userController.js";
 import express from "express";
 
 const router=express.Router();
 
 router.post("/signup",signup);
-
+router.post("/login",login);
 
 export default router;
