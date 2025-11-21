4ce240733e2091e3f4b9fed76b6117831235a935
AFZAL REYAZ <afjalreyaz@gmail.com>
2025-11-21 11:59:03 +0530

    cors comt

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+
 export const signup = async (req, res) => {
   try {
     const { name, mobile, password } = req.body;
